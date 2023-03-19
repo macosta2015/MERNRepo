d3c46fa0fb3ad89db3836ff8ca9f56bc2cdccd7e
macosta2015 <macosta2015@my.fit.edu>
2023-03-18 23:48:25 -0400

    commit

--- routes/hotels.js
+++ routes/hotels.js
@@ -31,12 +31,12 @@ router.put("/:id", async (req, res) => {
 })
 
 //DELETE
-router.put("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
     try {
         const updatedHotel = await Hotel.findByIdAndDelete(
             req.params.id
         );
-        res.status(200).json(updatedHotel);
+        res.status(200).json("Hotel has been deleted");
     } catch (err) {
         res.status(500).json(err)
     }
