a3c9eb39b51e99c3006106d6641bd063ce53be5e
macosta2015 <macosta2015@my.fit.edu>
2023-03-18 21:02:59 -0400

    commit

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
 
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to Mongo")
+    } catch (error) {
+        throw error
+        handleError(error);
+        console.log("We had an Error connecting to MongoDB")
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected!")
+})
+
+//Middlewares
+//We are taking the authRoute middlewares from the routes folder.
+app.use(express.json())
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, first request")
+})
 
 app.listen(8800, () => {
+    connect()
     console.log("Connectd to backend. ")
 })
