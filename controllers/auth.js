70f01ecde375f48414ec82e8246dd54338694863
macosta2015 <macosta2015@my.fit.edu>
2023-03-22 18:08:30 -0400

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt";
+// import { createError } from '../utils/error';
 
 export const register = async (req, res, next) => {
     try {
@@ -10,30 +11,22 @@ export const register = async (req, res, next) => {
             username: req.body.username,
             email: req.body.email,
             password: hash,
-        })
+        });
 
-        await newUser.save()
+        await newUser.save();
         res.status(200).send("User had been created. ")
     } catch (err) {
         next(err)
     }
-}
+};
 
 export const login = async (req, res, next) => {
     try {
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
-
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
-        })
-
-        await newUser.save()
-        res.status(200).send("User had been created. ")
+        const user = User.findOne({ username: req.body.username })
+        if (!user) return next(createError(404, "User not found! "))
+        res.status(200).send("User has been created");
     } catch (err) {
         next(err)
     }
-}
+};
 
