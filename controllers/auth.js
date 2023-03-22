6f73ed268165e01d670d3934040d601cecda0c34
macosta2015 <macosta2015@my.fit.edu>
2023-03-22 17:31:50 -0400

    Adding files into the controllers and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,33 @@
 import User from "../models/User.js"
+import bcrypt from "bcrypt";
 
 export const register = async (req, res, next) => {
     try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hash,
+        })
+
+        await newUser.save()
+        res.status(200).send("User had been created. ")
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
         })
 
         await newUser.save()
