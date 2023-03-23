ffd7c7ac1d47584386a9d1fad10b76bab20d9495
macosta2015 <macosta2015@my.fit.edu>
2023-03-22 23:27:26 -0400

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt";
-// import { createError } from '../utils/error';
+import { createError } from '../utils/error.js';
 
 export const register = async (req, res, next) => {
     try {
@@ -22,9 +22,14 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
     try {
-        const user = User.findOne({ username: req.body.username })
+        const user = await User.findOne({ username: req.body.username })
         if (!user) return next(createError(404, "User not found! "))
-        res.status(200).send("User has been created");
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"))
+
+        // res.status(200).send("User has been created");
+        res.status(200).json(user);
     } catch (err) {
         next(err)
     }
