f9eaa404e9669f2bb9b39754909b333e25c08fd7
macosta2015 <macosta2015@my.fit.edu>
2023-04-08 15:40:01 -0400

    Adding additional information to the controllers.

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,5 +1,6 @@
 //Data repets itself
 import Hotel from "../models/Hotel.js"
+import Room from "../models/Room.js";
 
 export const createHotel = async (req, res, next) => {
     const newHotel = new Hotel(req.body)
@@ -46,25 +47,31 @@ export const getHotel = async (req, res, next) => {
 }
 
 export const getHotels = async (req, res, next) => {
+    const { min, max, ...others } = req.query;
     try {
-        const hotels = await Hotel.find(req.query);
+        const hotels = await Hotel.find({
+            ...others,
+            cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+        }).limit(req.query.limit);
         res.status(200).json(hotels);
     } catch (err) {
-        next(err)
+        next(err);
     }
-}
+};
 
 export const countByCity = async (req, res, next) => {
-    const cities = req.query.cities.split(",")
+    const cities = req.query.cities.split(",");
     try {
-        const list = await Promise.all(cities.map(city => {
-            return Hotel.countDocuments({ city: city })
-        }))
+        const list = await Promise.all(
+            cities.map((city) => {
+                return Hotel.countDocuments({ city: city });
+            })
+        );
         res.status(200).json(list);
     } catch (err) {
-        next(err)
+        next(err);
     }
-}
+};
 
 export const countByType = async (req, res, next) => {
     try {
