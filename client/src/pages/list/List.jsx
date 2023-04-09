3798a0736c473bf6fc39366b19dce00a83c34914
macosta2015 <macosta2015@my.fit.edu>
2023-04-09 13:50:28 -0400

    Update List.jsx

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -14,8 +14,17 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, refetch } = useFetch(`/hotels?city=${destination}`)
+
+  const { data, loading, error, refetch } = useFetch(
+    `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`
+  );
+
+  const handleClick = () => {
+    refetch()
+  }
 
   return (
     <div>
@@ -50,13 +59,19 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input
+                    type="number"
+                    onChange={e => setMin(e.target.value)}
+                    className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input
+                    type="number"
+                    onChange={e => setMax(e.target.value)}
+                    className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -87,7 +102,7 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
           <div className="listResult">
             {loading ? "loading" : <>
