c8cc4cd3dad62fe00d0229a1bec04b6203fdc6b0
macosta2015 <macosta2015@my.fit.edu>
2023-04-12 19:53:37 -0400

    Update Reserve.jsx

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -4,10 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./reserve.css"
 
 import useFetch from "../../hooks/useFetch";
+import { useState } from 'react';
 
 const Reserve = ({ setOpen, hotelId }) => {
-
+  const [selectedRooms, setSelectedRooms] = useState([])
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+
+  const handleSelect = (e) => {
+    const selected = e.target.checked
+
+  }
 
   return (
     <div className="reserve">
@@ -26,7 +32,14 @@ const Reserve = ({ setOpen, hotelId }) => {
             <div className="rMax">
               Max people: <b>{item.maxPeople}</b>
             </div>
+            <div className="rPrice">{item.price}</div>
           </div>
+          {item.roomNumbers.map((roomNumber) => (
+            <div className='room'>
+              <label>{roomNumber.number}</label>
+              <input type="checkbox" value={roomNumber._id} onChange={handleSelect} />
+            </div>
+          ))}
         </div>
       ))}
     </div >
