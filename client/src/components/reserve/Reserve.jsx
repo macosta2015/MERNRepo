90fcdaac02e18bee4a36cfb2215dd0aacc84ae00
macosta2015 <macosta2015@my.fit.edu>
2023-04-12 19:12:51 -0400

    Update Reserve.jsx

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -18,6 +18,16 @@ const Reserve = ({ setOpen, hotelId }) => {
           onClick={() => setOpen(false)}
         />
         <span>Select your rooms:</span>
+        {data.map(item => (
+          <div className="rItem">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">Max People: <b>{item.maxPeople}</b>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
