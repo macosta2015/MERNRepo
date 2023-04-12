de3d7185a3ba4026110f02893a3105013b8f6396
macosta2015 <macosta2015@my.fit.edu>
2023-04-12 19:32:48 -0400

    Update Reserve.jsx

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -11,26 +11,25 @@ const Reserve = ({ setOpen, hotelId }) => {
 
   return (
     <div className="reserve">
-      <div className="rContainer">
-        <FontAwesomeIcon
-          icon={faCircleXmark}
-          className="rClose"
-          onClick={() => setOpen(false)}
-        />
-        <span>Select your rooms:</span>
-        {data.map((item) => (
-          <div className="rItem">
-            <div className="rItemInfo">
-              <div className="rTitle">{item.title}</div>
-              <div className="rDesc">{item.desc}</div>
-              <div className="rMax">
-                Max people: <b>{item.maxPeople}</b>
-              </div>
+      <div className="rContainer"></div>
+      <FontAwesomeIcon
+        icon={faCircleXmark}
+        className="rClose"
+        onClick={() => setOpen(false)}
+      />
+      <span>Select your rooms:</span>
+      {data.map((item) => (
+        <div className="rItem">
+          <div className="rItemInfo">
+            <div className="rTitle">{item.title}</div>
+            <div className="rDesc">{item.desc}</div>
+            <div className="rMax">
+              Max people: <b>{item.maxPeople}</b>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      ))}
+    </div >
   );
 };
 
